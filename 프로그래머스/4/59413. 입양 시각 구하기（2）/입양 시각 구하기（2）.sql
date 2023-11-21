7e6fa28afc5916264349f1731b9a5e2efc02507f
Saehan <wjdtpgks388@gmail.com>
2023-11-21 17:31:24 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,38 +1,47 @@
--- 코드를 입력하세요
-SELECT
-    hours.Number AS HOUR,
-    COALESCE(COUNT(DATE_FORMAT(o.DATETIME, '%H')), 0) AS COUNT
-FROM
-    (
-        SELECT 0 AS Number 
-        UNION 
-        SELECT 1 UNION ALL
-        SELECT 2 UNION ALL
-        SELECT 3 UNION ALL
-        SELECT 4 UNION ALL
-        SELECT 5 UNION ALL
-        SELECT 6 UNION ALL
-        SELECT 7 UNION ALL
-        SELECT 8 UNION ALL
-        SELECT 9 UNION ALL
-        SELECT 10 UNION ALL
-        SELECT 11 UNION ALL
-        SELECT 12 UNION ALL
-        SELECT 13 UNION ALL
-        SELECT 14 UNION ALL
-        SELECT 15 UNION ALL
-        SELECT 16 UNION ALL
-        SELECT 17 UNION ALL
-        SELECT 18 UNION ALL
-        SELECT 19 UNION ALL
-        SELECT 20 UNION ALL
-        SELECT 21 UNION ALL
-        SELECT 22 UNION ALL
-        SELECT 23
-    ) AS hours
-LEFT JOIN
-    animal_outs o ON hours.Number = HOUR(o.DATETIME)
-GROUP BY
-    hours.Number
-ORDER BY
-    hours.Number;
+# -- 코드를 입력하세요
+# SELECT
+#     hours.Num AS HOUR,
+#     COALESCE(COUNT(DATE_FORMAT(o.DATETIME, '%H')), 0) AS COUNT
+# FROM
+#     (
+#         SELECT 0 AS Num UNION 
+#         SELECT 1 UNION 
+#         SELECT 2 UNION 
+#         SELECT 3 UNION 
+#         SELECT 4 UNION 
+#         SELECT 5 UNION 
+#         SELECT 6 UNION 
+#         SELECT 7 UNION 
+#         SELECT 8 UNION 
+#         SELECT 9 UNION 
+#         SELECT 10 UNION 
+#         SELECT 11 UNION 
+#         SELECT 12 UNION 
+#         SELECT 13 UNION 
+#         SELECT 14 UNION 
+#         SELECT 15 UNION 
+#         SELECT 16 UNION 
+#         SELECT 17 UNION 
+#         SELECT 18 UNION 
+#         SELECT 19 UNION 
+#         SELECT 20 UNION 
+#         SELECT 21 UNION 
+#         SELECT 22 UNION 
+#         SELECT 23
+#     ) AS hours
+# LEFT JOIN
+#     animal_outs o ON hours.Num = HOUR(o.DATETIME)
+# GROUP BY
+#     hours.Num
+# ORDER BY
+#     hours.Num;
+WITH recursive cte AS (
+    SELECT 0 AS HOUR
+    UNION ALL
+    SELECT HOUR + 1 FROM cte WHERE HOUR <= 22
+)
+
+SELECT c.HOUR, COUNT(DATE_FORMAT(o.DATETIME, '%H')) AS COUNT
+FROM cte c
+LEFT JOIN ANIMAL_OUTS o ON c.HOUR = DATE_FORMAT(o.DATETIME, '%H')
+GROUP BY c.HOUR;
