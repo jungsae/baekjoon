9c7480ed15352c3494b62fb1f27df452fccfda31
Saehan <wjdtpgks388@gmail.com>
2023-12-13 17:20:42 +0900

    [level 2] Title: 더 맵게, Time: 1670.26 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -20,3 +20,37 @@ class Solution {
         return (pq.peek() >= K) ? acc : -1;
     }
 }
+// import java.util.ArrayList;
+// import java.util.Collections;
+
+// class Solution {
+//     public int solution(int[] scoville, int K) {
+//         int acc = 0;
+//         ArrayList<Integer> list = new ArrayList<>();
+
+//         for (int s : scoville) {
+//             list.add(s);
+//         }
+
+//         while (!list.isEmpty()) {
+//             Collections.sort(list);
+//             int first = list.remove(0);
+
+//             if (first >= K) {
+//                 return acc;
+//             }
+
+//             if (list.isEmpty()) {
+//                 return -1;
+//             }
+
+//             int second = list.remove(0);
+//             int newScoville = first + (second * 2);
+//             list.add(newScoville);
+//             acc++;
+//         }
+
+//         return -1;
+//     }
+// }
+
