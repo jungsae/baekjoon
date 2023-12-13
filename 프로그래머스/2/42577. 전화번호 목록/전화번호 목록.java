9ea37c28a714a2e1d8d319dcef293ffee5701b6f
Saehan <wjdtpgks388@gmail.com>
2023-12-13 11:42:25 +0900

    [level 2] Title: 전화번호 목록, Time: 331.33 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,16 +2,11 @@ import java.util.Arrays;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        // 전화번호 배열을 정렬
         Arrays.sort(phone_book);
-        
-        // 정렬된 배열에서 현재 전화번호와 다음 전화번호 비교
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
+        for (int i=0; i<phone_book.length-1; i++)
+            if (phone_book[i+1].startsWith(phone_book[i]))
                 return false;
-            }
-        }
         
-        return true;
+    return true;
     }
 }
