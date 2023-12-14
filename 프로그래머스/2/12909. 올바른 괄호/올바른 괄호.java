43ab0399d2ae4a1b0bc583915649b9396936238e
Saehan <wjdtpgks388@gmail.com>
2023-12-14 10:38:52 +0900

    [level 2] Title: 올바른 괄호, Time: 9.61 ms, Memory: 68 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+class Solution {
+    boolean solution(String s) {
+        if (s.charAt(0) == ')')
+                return false;
+        if (s.charAt(s.length()-1) == '(')
+                return false;
+        
+        int temp=0;
+        
+         for (int i=0; i<s.length(); i++)
+         {
+            if (temp < 0) 
+                return false;
+
+            if (s.charAt(i) == '(') 
+                temp++;
+            else if (s.charAt(i) == ')')
+                temp--;
+        }
+        
+    return (temp!=0) ? false: true;
+    }
+}
