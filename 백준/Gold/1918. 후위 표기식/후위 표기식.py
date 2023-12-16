e6e0ad0605846fb4d1e0e16e796cdd6effa17a07
Saehan <wjdtpgks388@gmail.com>
2023-12-17 07:42:50 +0900

    [Gold II] Title: 후위 표기식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.py
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -6,37 +6,29 @@ def getNum(operator):
     else:
         return -1
 
-def infixToPostfix(u):
+def infix_to_postfix(expression):
     stack = []
     output = []
-
-    for i in u:
-        if i.isalpha():
-            output.append(i)
-        
-        # i가 ')'면 
-        elif i == '(':
-            stack.append(i)
-            
-        elif i == ')':
-            # 스택에서 '(' 부분까지 연산자만 다 출력으로 이동
+    
+    for char in expression:
+        if char.isalpha():
+            output.append(char)
+        elif char == '(':
+            stack.append(char)
+        elif char == ')':
             while stack and stack[-1] != '(':
                 output.append(stack.pop())
-            stack.pop()            
-            
-        # 숫자가 아닌 연산자가 들어왔을때
-        # 우선순위비교
-        else:
-            # i 보다 크면 앞쪽 다 출력으로 이동
-            while (stack and getNum(stack[-1]) >= getNum(i)):           
+            stack.pop()  # '('를 스택에서 제거
+        else:  # 연산자인 경우
+            while stack and getNum(stack[-1]) >= getNum(char):
                 output.append(stack.pop())
-            stack.append(i)           
+            stack.append(char)
 
     while stack:
         output.append(stack.pop())
-        
+
     return ''.join(output)
 
 u = input()
-result = infixToPostfix(u)
+result = infix_to_postfix(u)
 print(result)
