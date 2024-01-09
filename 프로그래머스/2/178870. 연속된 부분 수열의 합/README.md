545feba697d644719f298a54dc8d1a193be75c8a
Saehan <wjdtpgks388@gmail.com>
2024-01-10 01:35:39 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속된 부분 수열의 합 - 178870 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870) 
 
 ### 성능 요약
 
-메모리: 74.2 MB, 시간: 0.27 ms
+메모리: 78.1 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 17:5:36
+2024년 1월 3일 1:35:36
 
 ### 문제 설명
 
--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,45 +1,34 @@
 import java.util.*;
-class Sequence{
-    int start;
-    int end;
-    public Sequence(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-}
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        List<Sequence> answers = new ArrayList<>();
-        int[] answer = new int[2];
-        int sum=0;
-        int lt=0;
-        for (int rt = 0; rt < sequence.length; rt++) {
-            sum += sequence[rt];
-            if(sum==k) {
-                answers.add(new Sequence(lt, rt)); //7과 1 7의 경우
+        int answer[]=new int[2];
+
+        int l=0,r=0;
+        int sum=sequence[0];
+        int min = Integer.MAX_VALUE;
+        int len = sequence.length;
+
+        while(l < len && r<len)
+        {
+            if(sum==k && min > r - l)
+            {
+                min = r - l;
+                answer[0] = l;
+                answer[1] = r;
             }
-            while(sum>k){
-                sum -= sequence[lt];
-                lt++;
-                if(sum==k) {
-                    answers.add(new Sequence(lt, rt)); // 1 2 3 4의 경우
-                }
+
+            if(sum<k)
+            {
+                r++;
+                if(r<len)
+                    sum+=sequence[r];
+            }
+            else
+            {
+                sum -= sequence[l];
+                l++;
             }
         }
-        int minLength = Integer.MAX_VALUE;
-        int minIndex=0;
-        // 정답 후보들을 모두 돌면서 가장 짧은 길이 찾기
-        List<Sequence> minLenSequences = new ArrayList<>();
-        int answers_len = answers.size();
-        for (int i = 0; i < answers_len; i++) {
-            int sequence_len = answers.get(i).end - answers.get(i).start;
-            if(sequence_len < minLength) {
-                minLength = sequence_len;
-                minIndex = i;
-            }
-        }
-        answer[0] = answers.get(minIndex).start;
-        answer[1] = answers.get(minIndex).end;
         return answer;
     }
 }
